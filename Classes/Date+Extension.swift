b0dc959926925f5f2c00864ff6212b4548d2543e
Ali Zain <azain@foree.co>
2020-07-20 01:58:01 +0500

    fix PODS issues

---
+++ Classes/Date+Extension.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+public extension Date {
+    
+    func myFunc(){
+        print("my func")
+    }
+    
+}
---
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by Ali Zain on 20/07/2020.
+//  Copyright © 2020 Ali Zain. All rights reserved.
+//
+
+import UIKit
+import ExtensionLite
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Date().myFunc()
+    }
+
+
+}
+
