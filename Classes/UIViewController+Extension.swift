b5a9134f9c1883b67eddb1606e9d70a6a0771d18
Ali Zain <azain@foree.co>
2020-07-22 00:28:09 +0500

    View controller extension

--- Classes/UIViewController+Extension.swift
+++ Classes/UIViewController+Extension.swift
@@ -5,4 +5,24 @@
 //  Created by Ali Zain on 22/07/2020.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    // Not using static as it wont be possible to override to provide custom storyboardID then
+    class var storyboardID : String {
+        
+        //if your storyboard name is same as ControllerName uncomment it
+        //return "\(self)"
+        return "\(self)" + "_ID"
+        
+    }
+    
+    static func instantiate(fromAppStoryboard appStoryboard: AppStoryboard) -> Self {
+        return appStoryboard.viewController(viewControllerClass: self)
+    }
+    
+    static func className(controller:UIViewController) -> String {
+        return String.init(describing: controller.self)
+    }
+}
