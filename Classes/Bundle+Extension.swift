89f4dc1e999e865579673955ffe4f4241f4a31de
Ali Zain <azain@foree.co>
2020-07-22 01:48:22 +0500

    bundle extension added

--- Classes/Bundle+Extension.swift
+++ Classes/Bundle+Extension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension Bundle {
+    
+    static func loadView<T>(fromNib name: String, withType type: T.Type) -> T {
+        if let view = Bundle.main.loadNibNamed(name, owner: nil, options: nil)?.first as? T {
+            return view
+        }
+        
+        fatalError("Could not load view with type " + String(describing: type))
+    }
+}
